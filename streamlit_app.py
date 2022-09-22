ccf6295ff65deeffb488ac585ddeb548de012629
kapilpal-123 <114182507+kapilpal-123@users.noreply.github.com>
2022-09-23 00:40:24 +0530

    Update streamlit_app.py
    remove good night and add four new lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,10 @@ import streamlit
 
 streamlit.title('My Parents new healthy diner')
 
-streamlit.title('Good Night')
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & blueberry Oatmeal')
+
+streamlit.text('Kale, Spinach & Rocket Smoothie  ')
+
+streamlit.text('Hard-Boiled Free_Range Egg')
