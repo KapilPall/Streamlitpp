c16a85052d7400b33a4faa6bbe724e90268ea1a6
kapilpal-123 <114182507+kapilpal-123@users.noreply.github.com>
2022-09-23 22:51:13 +0530

    Update streamlit_app.py
    Adding Avocado

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,5 @@ streamlit.text('🥣 Omega 3 & blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie  ')
 
 streamlit.text('🐔 Hard-Boiled Free_Range Egg')
+
+streamlit.text('🥑🍞 Avocado Toast')
